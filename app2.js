976c9c67dc82a57b26dfd06f84730ab7b7cdff31
matthieubrient <brient.matthieu@gmail.com>
2018-12-12 14:27:41 -0500

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,25 +1,20 @@
-var express = require('express');
-var app = express();
-
+var express=require('express');
+var app=express();
 app.get('/',function(req,res)
 {
     res.send('Hello World!');
 });
-
 var server=app.listen(3000,function() {});
 
-app.route('/acklio1',get(function(req,res)
+app.route('/Node',get(function(req,res)
 {
-    res.send("Acklio training 1");
+    res.send("Tutorial on Node");
 });
 post(function(req,res)
 {
-    res.send("Acklio training 2");
+    res.send("Tutorial on Angular");
 });
 put(function(req,res)
 {
-    res.send('Welcome to Acklio training session');
+    res.send('Welcome to Guru99 Tutorials');
 }));
-
-var express=require('express');
-var app=express();
