17c31e2a80665f6a9f39806a8806cb6009fd559f
matthieubrient <brient.matthieu@gmail.com>
2018-12-12 14:25:17 -0500

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var app = express();
+
+app.get('/',function(req,res)
+{
+    res.send('Hello World!');
+});
+
+var server=app.listen(3000,function() {});
+
 app.route('/acklio1',get(function(req,res)
 {
     res.send("Acklio training 1");
@@ -12,3 +20,6 @@ put(function(req,res)
 {
     res.send('Welcome to Acklio training session');
 }));
+
+var express=require('express');
+var app=express();
